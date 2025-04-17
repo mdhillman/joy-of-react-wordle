a9dab2da514106a073e50f02fa7bc65a37c38b91
Michael Hillman <michael.hillman@gearset.com>
2025-04-17 16:04:45 +0100

    Initial react project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+        <p>Here is some text</p>
+    </div>
+  );
+}
+
+export default App;
